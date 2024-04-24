b47b23980e255566c9aace6e120156434c548649
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-25 01:10:06 +0700

    Handling multiple exceptions

--- main.py
+++ main.py
@@ -27,6 +27,25 @@ if __name__ == "__main__":
 
   # Catching specific exceptions
 
+  # try:
+  #   print("Enter the net sales for")
+
+  #   previous = float(input("- Previous period:"))
+  #   current = float(input("- Current period:"))
+
+  #   change = (current - previous) * 100 / previous
+
+  #   if change > 0:
+  #     result = f"Sales increase {abs(change)}%"
+  #   else:
+  #     result = f"Sales decrease {abs(change)}%"
+
+  #   print(result)
+  # except ValueError:
+  #   print("Error! Please enter a number for net sales")
+
+  # Handling multiple exceptions
+
   try:
     print("Enter the net sales for")
 
@@ -42,4 +61,8 @@ if __name__ == "__main__":
 
     print(result)
   except ValueError:
-    print("Error! Please enter a number for net sales")
+    print("Error! Please enter a number for net sales.")
+  except ZeroDivisionError:
+    print("Error! The prior net sales cannot be zero.")
+  except Exception as error:
+    print(error)
