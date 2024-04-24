f7b7ee9f4df7be48139a08cb6d7ca3b55bda365a
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-25 01:08:50 +0700

    Catching specific exceptions

--- main.py
+++ main.py
@@ -8,6 +8,25 @@ if __name__ == "__main__":
     # handle error
   """
 
+  # try:
+  #   print("Enter the net sales for")
+
+  #   previous = float(input("- Previous period:"))
+  #   current = float(input("- Current period:"))
+
+  #   change = (current - previous) * 100 / previous
+
+  #   if change > 0:
+  #     result = f"Sales increase {abs(change)}%"
+  #   else:
+  #     result = f"Sales decrease {abs(change)}%"
+
+  #   print(result)
+  # except:
+  #   print("Error! Please enter a number for net sales")
+
+  # Catching specific exceptions
+
   try:
     print("Enter the net sales for")
 
@@ -22,5 +41,5 @@ if __name__ == "__main__":
       result = f"Sales decrease {abs(change)}%"
 
     print(result)
-  except:
+  except ValueError:
     print("Error! Please enter a number for net sales")
